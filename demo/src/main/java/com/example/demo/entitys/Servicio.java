9126d173d6eb0bfddcaa2d286151ec248b68af04
CarefreeHarp <daniramirezva@gmail.com>
2026-08-22 00:03:13 -0500

    [feat]: Crear entidad servicio y mock data 2

--- demo/src/main/java/com/example/demo/entitys/Servicio.java
+++ demo/src/main/java/com/example/demo/entitys/Servicio.java
@@ -1,6 +1,12 @@
 package com.example.demo.entitys;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Servicio {
 
     private int id;
@@ -10,61 +16,4 @@ public class Servicio {
     private String categoria;
     private boolean activo;
 
-    public Servicio(int id, String nombre, String descripcion, double precio,
-                    String categoria, boolean activo) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.categoria = categoria;
-        this.activo = activo;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(double precio) {
-        this.precio = precio;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
 }
