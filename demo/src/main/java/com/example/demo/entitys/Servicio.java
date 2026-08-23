24b1eb6c3288fc6b4883bca1a8c59d1d870ba041
CarefreeHarp <daniramirezva@gmail.com>
2026-08-23 00:33:01 -0500

    mock-data

--- demo/src/main/java/com/example/demo/entitys/Servicio.java
+++ demo/src/main/java/com/example/demo/entitys/Servicio.java
@@ -3,6 +3,7 @@ package com.example.demo.entitys;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -15,5 +16,12 @@ public class Servicio {
     private double precio;
     private String categoria;
     private boolean activo;
+    private String resumen;
+    private String duracion;
+    private String disponibilidad;
+    private String ubicacion;
+    private String imagenPrincipalUrl;
+    private List<String> imagenesGaleriaUrls;
+    private List<String> beneficios;
 
 }
