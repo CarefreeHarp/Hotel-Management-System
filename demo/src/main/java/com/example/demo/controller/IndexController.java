cff3f1a07f79d17030b208b8ff76aee0f63ae630
jsanchez312 <morganfryman420@gmail.com>
2026-08-25 09:48:53 -0500

    servicios vista por tarjetas

--- demo/src/main/java/com/example/demo/controller/IndexController.java
+++ demo/src/main/java/com/example/demo/controller/IndexController.java
@@ -5,10 +5,45 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    
 
-    @GetMapping("/index")
-    public String index(){
+    @GetMapping({"/", "/index", "/home"})
+    public String index() {
         return "index";
     }
+
+    @GetMapping("/suites")
+    public String suites() {
+        return "redirect:/#rooms-section";
+    }
+
+    @GetMapping("/book-now")
+    public String bookNow() {
+        return "redirect:/#book-section";
+    }
+
+    @GetMapping("/experiences")
+    public String experiences() {
+        return "redirect:/#experiences-section";
+    }
+
+    @GetMapping("/getting-here")
+    public String gettingHere() {
+        return "redirect:/#book-section";
+    }
+
+    @GetMapping("/explore")
+    public String explore() {
+        return "redirect:/#video-section";
+    }
+
+    @GetMapping("/restaurant")
+    public String restaurant() {
+        return "redirect:/#services-title";
+    }
+
+    @GetMapping("/awards")
+    public String awards() {
+        return "redirect:/#hero";
+    }
 }
+
