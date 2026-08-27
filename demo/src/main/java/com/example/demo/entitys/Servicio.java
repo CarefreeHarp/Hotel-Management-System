f1966bd6a6765018671e3204f42b624191c5c5b6
guille-011 <apontecardenas54321@gmail.com>
2026-08-27 11:50:51 -0500

    Entitys and CRUD
    This commit contains the creation of the entitys needed for the sprint, these are: Cliente, EstadoHabitacion, Habitacion and TipoHabitacion. It also added the CRUD logic for the entitys cliente and TipoHabitacion

--- demo/src/main/java/com/example/demo/entitys/Servicio.java
+++ demo/src/main/java/com/example/demo/entitys/Servicio.java
@@ -1,9 +1,10 @@
 package com.example.demo.entitys;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -26,3 +27,5 @@ public class Servicio {
     private List<String> beneficios;
 
 }
+
+
